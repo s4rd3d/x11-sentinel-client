3c5fc13c8fff8af6430a89c551751ee601933e87
Sándor Dávid <sandordavid1124@gmail.com>
2022-04-17 14:09:07 +0200

    feat: Add `x11rb` dependency and print X protocol version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use x11rb::connection::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let (conn, _number_of_screens) = x11rb::connect(None).unwrap();
+    let setup = &conn.setup();
+    println!(
+        "X Protocol version: {}.{}",
+        setup.protocol_major_version, setup.protocol_minor_version
+    );
 }
